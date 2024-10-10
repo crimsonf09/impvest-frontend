b7c2619803baee0f2b25d9cb840be6084ce8fe7f
crimson <123491192+crimsonf09@users.noreply.github.com>
2024-10-10 15:54:20 +0000

    test3

--- app/page.tsx
+++ app/page.tsx
@@ -78,8 +78,8 @@ export default function Home() {
               -&gt;
             </span>
           </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
+          <p className={`m-0 max-w-[30ch] text-xl opacity-50`}>
+            yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy{" "}
           </p>
         </a>
 
